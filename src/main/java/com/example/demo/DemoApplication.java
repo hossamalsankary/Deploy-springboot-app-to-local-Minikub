f4db0241cedfe679bfce182e26223211b18b9cf4
hossamalsankary <hossamalsankary@gmail.com>
2022-12-18 23:57:50 +0200

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public static void main(String[] args) {
 SpringApplication.run(DemoApplication.class, args);
 }
 
-@GetMapping("/hello")
+@GetMapping("/")
 public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 return String.format("Hello %s!", name);
 }
