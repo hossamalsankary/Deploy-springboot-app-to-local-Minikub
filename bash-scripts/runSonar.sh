88aa72f6366ea8b36d511bf3f70a8ec1d00e4a00
hossamalsankary <hossamalsankary@gmail.com>
2022-12-21 11:24:57 +0200

    update

--- bash-scripts/runSonar.sh
+++ bash-scripts/runSonar.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-./gradlew sonar \
+./gradlew sonarqube \
 -Dsonar.projectKey=k \
 -Dsonar.host.url=http://ec2-3-140-251-146.us-east-2.compute.amazonaws.com:9000 \
--Dsonar.login=sqp_cebaf350bec84785026eb5bed8fb9a9203b95724 /
-docker run \
-    --rm \
-    -e SONAR_HOST_URL="http://ec2-3-140-251-146.us-east-2.compute.amazonaws.com:9000 " \
-    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=29993"
-    -e SONAR_LOGIN="sqp_cebaf350bec84785026eb5bed8fb9a9203b95724" \
-    -v "${PWD}:/usr/src" \
-    sonarsource/sonar-scanner-cli
+-Dsonar.login=sqp_cebaf350bec84785026eb5bed8fb9a9203b95724 \
