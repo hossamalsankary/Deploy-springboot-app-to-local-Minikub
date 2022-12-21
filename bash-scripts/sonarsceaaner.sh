d146605291d122719cb51183ca35254616990493
hossamalsankary <hossamalsankary@gmail.com>
2022-12-21 12:08:52 +0200

    update Master

--- bash-scripts/sonarsceaaner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget  -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
-unzip sonar-scanner-cli-4.7.0.2747-linux.zip
-export PATH="$PATH:$PWD/sonar-scanner/bin"
