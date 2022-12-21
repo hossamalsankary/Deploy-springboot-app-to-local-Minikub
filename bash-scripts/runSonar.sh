0deb13b65900ace35eb90e6e1997772b7516c8dd
hossamalsankary <hossamalsankary@gmail.com>
2022-12-21 11:13:27 +0200

    update

--- bash-scripts/runSonar.sh
+++ bash-scripts/runSonar.sh
@@ -3,3 +3,4 @@
 -Dsonar.projectKey=k \
 -Dsonar.host.url=http://ec2-3-140-251-146.us-east-2.compute.amazonaws.com:9000 \
 -Dsonar.login=sqp_cebaf350bec84785026eb5bed8fb9a9203b95724
+-Dsonar.branch.autoconfig.disabled=true
